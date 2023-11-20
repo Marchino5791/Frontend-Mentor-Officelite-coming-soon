8985311e4d5cafe577064cf7b373b76c834026ac
Marco Paolino <108570157+Marchino5791@users.noreply.github.com>
2023-11-20 13:33:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Frontend-Mentor-Officelite-coming-soon/',
+  plugins: [react()]
 })
