865fa2f9952e172f8bb822465b68e28a03f172fa
Marco Paolino <marcoantonio.paolino@gmail.com>
2023-11-21 11:22:42 +0100

    logo link to home

--- src/components/logo/logo.component.jsx
+++ src/components/logo/logo.component.jsx
@@ -1,12 +1,14 @@
 import logo from '../../assets/shared/logo.svg'
 import './logo.styles.scss'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Logo = () => {
   return (
     <>
       <div className="logo-container">
-        <img src={logo} alt="Officelite's Logo" />
+        <Link to="/Frontend-Mentor-Officelite-coming-soon">
+          <img src={logo} alt="Officelite's Logo" />
+        </Link>
       </div>
       <Outlet />
     </>
